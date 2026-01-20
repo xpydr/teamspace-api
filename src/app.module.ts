ef23980cfba927c2452e97d7ad29ce6770d41682
Rohan Puri <purirohan62@gmail.com>
2026-01-20 13:02:17 -0500

    feat: add base modules
    - users, teams, tasks, auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TeamsModule } from './teams/teams.module';
+import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TeamsModule, TasksModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
